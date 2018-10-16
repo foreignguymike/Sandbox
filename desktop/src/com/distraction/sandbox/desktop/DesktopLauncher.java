99becc483825a8c87b9672726c69fda0ef8866d6
Mike S <foreign.guy.mike@gmail.com>
2018-10-16 17:48:04 -0400

    Initial commit

---
+++ desktop/src/com/distraction/sandbox/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.distraction.sandbox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.distraction.sandbox.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
